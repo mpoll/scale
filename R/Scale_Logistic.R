941e48eb6c12baad28c32dc32ff97076fc1403d6
Unknown <m.pollock@warwick.ac.uk>
2018-04-26 12:15:01 +0100

    Initialisation and Seed Saving
    Modification to package to allow saving of initialisation values (alpha etc. and gradient bounds), and seeds for reconstructing prematurely halted executions.

--- R/Scale_Logistic.R
+++ R/Scale_Logistic.R
@@ -92,6 +92,12 @@ data.extrema    <- function(){
     list(dim.grad.max=dim.grad.max)}
 
 ########################################################################
+#### 3.3 - Initialise ScaLE algorithm
+########################################################################
+
+scale_logistic_init <- function(fnm="logistic_default.RData"){data.init(); data.extrema(); save(file=fnm,dim.grad.max=dim.grad.max,alpha.cent=alpha.cent,alpha.cent.sq=alpha.cent.sq,alpha.p.cent=alpha.p.cent,phi.cent=phi.cent)}
+
+########################################################################
 ########################################################################
 ### 4 - Functional to execute exact version of ScaLE for logistic data
 ########################################################################
@@ -99,9 +105,10 @@ data.extrema    <- function(){
 
 scale_logistic <- function(fnm="logistic_default.RData",p.num=2^10,t.inc=0.1,T.extend=0.1,run.length=100000,ss.size=10,ss.on=TRUE,seed.default=1,default.data=TRUE){
     set.seed(seed.default)
+    save(file=fnm,dim.grad.max=dim.grad.max,alpha.cent=alpha.cent,alpha.cent.sq=alpha.cent.sq,alpha.p.cent=alpha.p.cent,phi.cent=phi.cent,curr.seed=.Random.seed)
     if(default.data==TRUE){big.logistic.example()}else{user.logistic.example()}
     time.elapse <- system.time(simn <<- scale_exact(p.num=p.num,t.inc=t.inc,T.fin=T.extend,ss.phi=ss.phi,ss.phiC=ss.phiC,dimen,scale.transform,un.scale.transform,T.start=0,x.init=NULL,ss.size=ss.size,ess.thresh=0.5,resamp.method=resid.resamp,neg.wei.mech=scale_zero.wei,prev.simn=NULL,progress.check=FALSE,phi.record=FALSE,resamp.freq=p.num-1,theta=NULL,p.path.renew=p.path.renew))[3]
-    save(file=fnm,simn=simn,time.elapse=time.elapse); print(time.elapse)
+    save(file=fnm,simn=simn,dim.grad.max=dim.grad.max,alpha.cent=alpha.cent,alpha.cent.sq=alpha.cent.sq,alpha.p.cent=alpha.p.cent,phi.cent=phi.cent,curr.seed=.Random.seed,time.elapse=time.elapse); print(time.elapse)
     while(simn$T.fin < run.length){time.inc <- system.time(simn <<- scale_extend(simn,t.inc,T.extend))[3]
     time.elapse <- time.elapse + time.inc
         save(file=fnm,simn,time.elapse); print(time.elapse)}}
